14d4fc72f3d7e70bb21f12d8787a83e4e200d74e
Cody Sork <codysork@pm.me>
2022-03-11 16:52:21 -0800

    Added exception handling

--- gptwilio/twilio_client.py
+++ gptwilio/twilio_client.py
@@ -4,16 +4,17 @@ from twilio.rest import Client as TwilioClient
 class EasyClient(TwilioClient):
     """A Twilio client that is easier to work with."""
 
+    class CannotSend(Exception):
+        pass
+
     def __init__(self, phone_number: str, text: str):
         self.phone_number = phone_number
         self.text = text
 
     def sms(self, phone_number: str = None, text: str = None) -> object:
         """Sends a text message to a specified phone number."""
-        if phone_number:
-            phone_number = self.phone_number
-        if text:
-            text = self.text
+        if not phone_number or not text:
+            throw CannotSend()
         return self.messages.create(
             messaging_service_sid='NEEDS_TO_BE_REPLACED',
             body=text,
