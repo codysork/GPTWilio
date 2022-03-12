aa54fde5866dd725a034e200b347e96ab29ec99c
Cody Sork <codysork@pm.me>
2022-03-11 16:42:23 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='GPTWilio',
+    version='0.1',
+    packages=['gptwilio'],
+    url='https://github.com/codysork/GPTWilio',
+    license='GPL-3',
+    author='Cody Sork',
+    author_email='codysork@gmail.com',
+    description='A python library for sending ai-generated text messages'
+)
