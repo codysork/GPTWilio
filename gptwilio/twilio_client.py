c708fa324bc1b9f686457d0344618cdab50f821e
Cody Sork <codysork@pm.me>
2022-03-11 16:59:42 -0800

    Updated error handling and refactored "text" to "text_message"

--- gptwilio/twilio_client.py
+++ gptwilio/twilio_client.py
@@ -7,15 +7,23 @@ class EasyClient(TwilioClient):
     class CannotSend(Exception):
         pass
 
-    def __init__(self, phone_number: str, text: str):
+    def __init__(self, phone_number: str = None, text_message: str = None):
         self.phone_number = phone_number
-        self.text = text
+        self.text_message = text_message
 
-    def sms(self, phone_number: str = None, text: str = None) -> object:
+    def sms(self, phone_number: str = None, text_message: str = None) -> object:
         """Sends a text message to a specified phone number."""
-        if not phone_number or not text:
-            raise CannotSend()
+        if not phone_number:
+            if self.phone_number:
+                phone_number = self.phone_number
+            else:
+                raise self.CannotSend()
+            if not self.text_message:
+                raise self.CannotSend()
+        if not text_message:
+            raise self.CannotSend()
+
         return self.messages.create(
             messaging_service_sid='NEEDS_TO_BE_REPLACED',
-            body=text,
+            body=text_message,
             to=phone_number)
